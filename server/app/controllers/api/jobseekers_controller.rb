cac1d703dac27a2011b72b7e8c3533389abc8108
ITOSHINN <shinn2003@au.com>
2025-10-31 17:25:19 +0900

    新規登録時に仮登録の場合バリデーションエラーを起こさず再送するようにした

--- server/app/controllers/api/jobseekers_controller.rb
+++ server/app/controllers/api/jobseekers_controller.rb
@@ -1,4 +1,19 @@
 class Api::JobseekersController < DeviseTokenAuth::RegistrationsController
+
+  def create
+    existing = JobSeeker.find_by(email: sign_up_params[:email])
+
+    unless existing&.confirmed? 
+      existing.send_confirmation_instructions
+      render json: {
+        status: "success"
+      }
+      return
+    end
+    
+    super
+  end
+
   private
 
   def sign_up_params
